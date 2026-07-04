77ec59e449853dfe7a60925b1a34b00307079427
sota <soutanarisawa200795nari@gmail.com>
2026-07-04 16:19:26 +0900

    改良

--- save_clip.php
+++ save_clip.php
@@ -1,32 +1,16 @@
 <?php
 
-$file = "db.php";
-
-if (!file_exists($file)) {
-echo "clips.jsonがありません";
-exit;
-}
-
-$data = json_decode(file_get_contents($file), true);
-
-$id = $_POST["id"] ?? "";
-$text = $_POST["text"] ?? "";
-
-$data[$id] = $text;
-
-<?php
-
 require "db.php";
 
-$id=$_POST["id"];
-$text=$_POST["text"];
+$id = $_POST["id"];
+$text = $_POST["text"];
 
-$stmt=$db->prepare("
+$stmt = $db->prepare("
 UPDATE clips
-SET text=?
-WHERE id=?
+SET text = ?
+WHERE id = ?
 ");
 
-$stmt->execute([$text,$id]);
+$stmt->execute([$text, $id]);
 
 echo "保存成功";
